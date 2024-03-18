a2aabf7648ac34c9b5cdb01abd050ee776df37e4
芝風 <shibafuu@shibafuus-MacBook-Air.local>
2024-03-18 20:45:46 +0900

    sample test message

--- views/register.php
+++ views/register.php
@@ -46,7 +46,7 @@
 
                     </form>
                     <p class="text-center mt-3 small">Registered already? <a href="../views"> Login</a></p>
-                    <div class="text-center mt-3 small">kredo @ 2024</div>
+                    <div class="text-center mt-3 small">kredo @ 2024 * Test2</div>
                 </div>
             </div>
         </div>
